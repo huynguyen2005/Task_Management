a8fdfa1dcbe8d14dc7312b22bf49053641e67af3
NguyenVanHuy205 <nguyenvanhuy12072005@gmail.com>
2025-11-22 16:48:24 +0700

    Tạo dự án

---
+++ models/task.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+    title: String,
+    status: String,
+    content: String,
+    timeStart: Date,
+    timeFinish: Date,
+    deleted: {
+        type: Boolean,
+        default: false
+    },
+    deletedAt: Date
+}, {
+    timestamps: true
+}); //Tạo mới 1 cái bộ khung, khuôn mẫu 
+
+const Task = mongoose.model('Task', taskSchema, "tasks"); //Khởi tạo nó
+
+//Tham số 1: Tên model, tham số 2: tên schema mà định nghĩa ở trên, tham số 3 là tên collection bên trong mongodb
+
+module.exports = Task;
