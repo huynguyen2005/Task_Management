057f7d20af99f8f4cf7c838eef1aec8dc100566b
NguyenVanHuy205 <nguyenvanhuy12072005@gmail.com>
2025-11-22 21:29:28 +0700

    Tính năng phân trang

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -1,4 +1,5 @@
 const Task = require("../models/task.model");
+const paginationHelper = require("../../../helpers/pagination");
 
 //[GET] /api/v1/tasks
 module.exports.index =  async (req, res) => {
@@ -7,14 +8,23 @@ module.exports.index =  async (req, res) => {
             deleted: false
         };
         const sort = {};
+
+        //Lọc công việc theo trạng thái
         if(req.query.status){
             find.status = req.query.status;
         }
+
+        //Sắp xếp công việc theo tiêu chí
         if (req.query.sortKey && req.query.sortValue) {
             sort[req.query.sortKey] = req.query.sortValue;
         }
 
-        const tasks = await Task.find(find).sort(sort);
+        //Phân trang
+        const totalRecord = await Task.countDocuments(find);
+        const initPagination = paginationHelper(totalRecord, req.query.page);
+        
+
+        const tasks = await Task.find(find).sort(sort).limit(initPagination.limitRecord).skip(initPagination.skip);
         res.json(tasks);
     } catch (error) {
         res.json("Không tìm thấy");
