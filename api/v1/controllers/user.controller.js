d8cec309c100cafb5790290b2da7cc7c09f816cb
NguyenVanHuy205 <nguyenvanhuy12072005@gmail.com>
2025-11-23 20:45:09 +0700

    Tính năng quên mật khẩu

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -1,10 +1,13 @@
 const User = require("../models/user.model");
+const ForgotPassword = require("../models/forgot-password.model");
 const md5 = require('md5');
+const generateHelper = require("../../../helpers/generate");
+const sendMailHelper = require("../../../helpers/sendMail");
 
 //[POST] /users/register
 module.exports.register = async (req, res) => {
     try {
-        const exitUser = await User.findOne({ email: req.body.email, deleted: false});
+        const exitUser = await User.findOne({ email: req.body.email, deleted: false });
         if (exitUser) {
             return res.json({
                 code: 400,
@@ -26,3 +29,143 @@ module.exports.register = async (req, res) => {
         });
     }
 }
+
+//[POST] /users/login
+module.exports.login = async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email, deleted: false });
+        if (!user) {
+            return res.json({
+                code: 400,
+                message: "Email không tồn tại"
+            });
+        }
+        if (user.password != md5(req.body.password)) {
+            return res.json({
+                code: 400,
+                message: "Sai mật khẩu"
+            });
+        }
+        if (user.status == "inactive") {
+            return res.json({
+                code: 400,
+                message: "Tài khoản đã bị khóa"
+            });
+        }
+        res.cookie("token", user.tokenUser);
+        res.json({
+            code: 200,
+            mesage: "Đăng nhập thành công"
+        });
+    } catch (error) {
+        res.json({
+            code: 400,
+            mesage: error
+        });
+    }
+}
+
+//[POST] /users/password/forgot
+module.exports.forgotPassword = async (req, res) => {
+    try {
+        //kiểm tra xem email có tồn tại không
+        const exitUser = await User.findOne({ email: req.body.email, deleted: false });
+        if (!exitUser) {
+            return res.json({
+                code: 400,
+                message: "Email không tồn tại"
+            });
+        }
+
+        //kiểm tra email này đã được gửi mã chưa
+        const exitOTP = await ForgotPassword.findOne({ email: req.body.email });
+        if (exitOTP) {
+            return res.json({
+                code: 400,
+                message: "Email đã được gửi mã OTP"
+            });
+        }
+        const otp = generateHelper.generateRandomNumber(4);
+
+        //Gửi mã OTP qua Mail
+        const html = `
+        <p>Mã OTP là: <b>${otp}</b> .Thời gian sử dụng là 3 phút</p>
+    `;
+        sendMailHelper(req.body.email, "Lấy lại mật khẩu", html);
+
+        //Lưu vào db
+        const forgotPassword = new ForgotPassword({
+            email: req.body.email,
+            otp: otp,
+            expireAt: new Date()
+        });
+        await forgotPassword.save();
+
+        res.json({
+            code: 200,
+            email: req.body.email
+        });
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: error
+        });
+    }
+}
+
+//[POST] /users/password/otp
+module.exports.otpPassword = async (req, res) => {
+    try {
+        const checkOTP = await ForgotPassword.findOne({ email: req.body.email, otp: req.body.otp });
+        if (!checkOTP) {
+            return res.json({
+                code: 400,
+                message: "Mã OTP không chính xác"
+            });
+        }
+        const user = await User.findOne({ email: req.body.email });
+        res.cookie("token", user.tokenUser);
+        res.json({
+            code: 200,
+            mesage: "Xác thực thành công",
+            token: user.tokenUser
+        });
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: error
+        });
+    }
+}
+
+//[POST] /users/password/reset
+module.exports.resetPassword = async (req, res) => {
+    const token = req.cookies.token;
+    const password = req.body.password;
+
+    try {
+        const user = await User.findOne({ tokenUser: token });
+        if (md5(password) == user.password) {
+            return res.json({
+                code: 400,
+                message: "Vui lòng nhập mật khẩu mới khác mật khẩu cũ"
+            });
+        }
+        await User.updateOne({
+            tokenUser: token
+        }, {
+            password: md5(password)
+        });
+        await ForgotPassword.deleteOne({email: user.email});
+        res.json({
+            code: 200,
+            message: "Đổi mật khẩu thành công"
+        });
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: "Đổi mật khẩu thất bại"
+        });
+    }
+}
+
